46310bea0c8df61262df3c5a71d6e09e7dcac121
haevn <nils.milewski@stud.hs-ruhrwest.de>
2021-06-18 00:02:27 +0200

    fix of display

--- wmm/index.php
+++ wmm/index.php
@@ -13,7 +13,7 @@
     <div class="sub_div">
         <h1 class="heading_color">Tetris</h1>
         <div class="horizontal_centered">
-        <div class="centered_container">
+        <div>
             <button class="custom_button custom_button_color" type="button" onclick="location.href='/game/game.php';">
               <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 16.5l6-4.5-6-4.5zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
             </button>
